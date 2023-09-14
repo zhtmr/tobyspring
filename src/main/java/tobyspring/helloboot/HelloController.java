b9a7d9596af7174fb876b080020cc0bdf6c6bfc5
zhtmr <zhtmr123@gmail.com>
2023-09-14 19:22:28 +0900

    dispatcherServlet 이 요청처리를 위해 컨트롤러 bean 을 찾는다.

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,13 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
 
+@RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
 
@@ -10,12 +15,10 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
-//        SimpleHelloService helloService = new SimpleHelloService();
-
         return helloService.sayHello(Objects.requireNonNull(name));
-
-
     }
 
 }

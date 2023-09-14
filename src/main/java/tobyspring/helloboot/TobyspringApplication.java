6192389534b486d507797d2a24d0ff4af7b2e321
zhtmr <zhtmr123@gmail.com>
2023-09-14 17:17:06 +0900

    dispatcherServlet 으로 변경

--- src/main/java/tobyspring/helloboot/TobyspringApplication.java
+++ src/main/java/tobyspring/helloboot/TobyspringApplication.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,36 +25,16 @@ public class TobyspringApplication {
     public static void main(String[] args)
     {
 
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory factory = new TomcatServletWebServerFactory();
         WebServer webServer = factory.getWebServer(servletContext -> {
-
-//            HelloController helloController = new HelloController();
-
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 인증, 보안, 다국어, 공통 기능 처리 후..
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-//                        resp.setStatus(HttpStatus.OK.value());
-//                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
         webServer.start();
     }

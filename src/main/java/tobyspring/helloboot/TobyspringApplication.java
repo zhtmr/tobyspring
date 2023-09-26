55f97d266ec8512ead5c09e04e3730922e97f3d0
zhtmr <zhtmr123@gmail.com>
2023-09-26 17:30:38 +0900

    자바코드 구성정보 사용 - 팩토리메서드 기반 코드로 작성 - @Bean 어노테이션으로 빈등록 - AnnotationConfigWebApplicationContext 로 수정 - @Configuration 으로 bean 구성정보, 설정정보 등을 가진 클래스 인식

--- src/main/java/tobyspring/helloboot/TobyspringApplication.java
+++ src/main/java/tobyspring/helloboot/TobyspringApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -20,12 +23,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
+@Configuration
 public class TobyspringApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args)
     {
         // 스프링 컨테이너를 초기화하면서 서블릿 컨테이너도 초기화하게 수정
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() { // 오버라이드를 통해 템플릿메서드 패턴 훅 재정의
                 super.onRefresh();
@@ -41,8 +54,8 @@ public class TobyspringApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+//        applicationContext.registerBean(HelloController.class);
+        applicationContext.register(TobyspringApplication.class);
         applicationContext.refresh();
 
     }
